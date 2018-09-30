a87ac260d8f061ea3fbff92ea6d47dd386e274e2
sanwancha <sanwancha@gmail.com>
2018-09-30 17:25:22 +0800

    小程序实战第六天

--- pages/posts/posts-detail/post-detail.js
+++ pages/posts/posts-detail/post-detail.js
@@ -14,10 +14,38 @@ Page({
    */
   onLoad: function (options) {
     var postid = options.id;
+    this.data.currentPostId = postid;
     var postData = postsData.postList[postid];
     this.setData({
       postDetailKey: postData
     });
+    var postsCollected = wx.getStorageSync("posts_collected");
+    if (postsCollected){
+      var postCollected = postsCollected[postid]
+      if (postCollected){
+        this.setData({
+          collected: postCollected
+        })
+      };
+    }else{
+      var postsCollected = {};
+      postsCollected[postid] = [false];
+      wx.setStorageSync("posts_collected", postsCollected)
+    }
+  },
+
+  onCollectionTap:function(event){
+    var postsCollected = wx.getStorageSync("posts_collected");
+    var postCollected = postsCollected[this.data.currentPostId];
+    //收藏变为未收藏，为收藏变为收藏
+    postCollected = !postCollected;
+    postsCollected[this.data.currentPostId] = postCollected;
+    // 更新文章是否的缓存值
+    wx.setStorageSync("posts_collected", postsCollected);
+    //更新数据绑定变量，从而实现切换图片
+    this.setData({
+      collected: postCollected
+    })
   },
 
   /**
